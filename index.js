be8c7e9c544c917246819bd5814fa99c04fb17eb
Rahil-Huseynov <rahil.huseynov.05@gmail.com>
2024-03-14 18:53:31 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// Task - 1 
+// const car = {
+//     made: 'BMW',
+//     model: 'F30',
+//     year: 2013,
+
+//     start() {
+//         console.log("Made", this.made)
+//         console.log("Model", this.model)
+//         console.log("Year", this.year)
+//     }
+// }
+
+// car.start()
+
+// // const car2 = {
+// //     made: 'Mercedes',
+// //     model: 'G class',
+// //     year: 2023
+// // }
+
+// car.start.bind(car2)()
+
+
+// Task - 2
+
+
+// const car = {
+//     made: 'BMW',
+//     model: 'F30',
+//     year: 2013,
+// }
+
+// console.log(car)
+
+// const car2 = Object.create(car)
+
+
+// console.log(car2)
